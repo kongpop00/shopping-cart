b531cac58165ee25a7dc83a9376d8dc4f72ef9d6
K-ongpop Nualsaard <kongpopku@ku.th>
2023-12-11 21:09:29 +0700

    first commit

---
+++ src/component/review/Rating.tsx
@@ -0,0 +1,29 @@
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import { DataReview } from "./dataReview";
+
+export default function BasicRating(props: any) {
+  const [value, setValue] = React.useState<number | null>(2);
+
+  const rating = DataReview.map((e) => e.id);
+  console.log(rating);
+
+  return (
+    <Box
+      sx={{
+        "& > legend": { mt: 1 },
+      }}
+    >
+      <Typography component="legend"></Typography>
+      <Rating
+        name="simple-controlled"
+        value={props.data}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+    </Box>
+  );
+}
